4d148fd7265a434ee9f679ceee2c388da4aecbdf
Moisés Córdova <kernel_linux@outlook.com>
2019-09-05 22:00:28 -0500

    Table view with collectionView TabBarController

---
+++ 1 - Interfaces/Interfaz-1/Interfaz-1/AlbumCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  AlbumCollectionViewCell.swift
+//  Interfaz-1
+//
+//  Created by Moisés Córdova on 9/5/19.
+//  Copyright © 2019 Moisés Córdova. All rights reserved.
+//
+
+import UIKit
+
+class AlbumCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var albumImageView: UIImageView!
+    @IBOutlet var artistLabel: UILabel!
+    
+}
---
+++ 1 - Interfaces/Interfaz-1/Interfaz-1/SongTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  SongTableViewCell.swift
+//  Interfaz-1
+//
+//  Created by Moisés Córdova on 9/5/19.
+//  Copyright © 2019 Moisés Córdova. All rights reserved.
+//
+
+import UIKit
+
+class SongTableViewCell: UITableViewCell {
+    @IBOutlet var albumImageView: UIImageView!
+    @IBOutlet var songLabel: UILabel!
+    @IBOutlet var albumLabel: UILabel!
+    @IBOutlet var timeLabel: UILabel!
+    
+}
