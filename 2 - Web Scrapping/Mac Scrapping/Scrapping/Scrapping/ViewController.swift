3cd8385a79ad025f4fd4fb88de6717ddca3442de
Moisés Córdova <kernel_linux@outlook.com>
2019-10-01 20:16:59 -0500

    Initial scrapping project for mac with pods

---
+++ 2 - Web Scrapping/Mac Scrapping/Scrapping/Scrapping/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Scrapping
+//
+//  Created by Moisés Córdova on 10/1/19.
+//  Copyright © 2019 Moisés Córdova. All rights reserved.
+//
+
+import Alamofire
+import Kanna
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
