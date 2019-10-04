a3eb581ea8bc8bfee69c6669fbdf57d979c9efc5
Moisés Córdova <kernel_linux@outlook.com>
2019-10-03 20:11:20 -0500

    Created first scrape for popular page Added settings for network sandboxing

--- 2 - Web Scrapping/Mac Scrapping/Scrapping/Scrapping/ViewController.swift
+++ 2 - Web Scrapping/Mac Scrapping/Scrapping/Scrapping/ViewController.swift
@@ -8,12 +8,15 @@
 
 import Alamofire
 import Kanna
+import Cocoa
 
 class ViewController: NSViewController {
+    
+    var urlString = "https://www.musixmatch.com/es"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        scrapeURL()
         // Do any additional setup after loading the view.
     }
 
@@ -23,6 +26,56 @@ class ViewController: NSViewController {
         }
     }
 
+    func scrapeURL() {
+        Alamofire.request(urlString).responseString { response in
+            if let htmlString = response.result.value {
+                self.parseResultsPopular(html: htmlString)
+            }
+        }
+    }
+    
+    func parseResultsPopular(html: String) {
+        do {
+            let htmlDocument = try Kanna.HTML(html: html, encoding: .utf8)
+            for ul in htmlDocument.css("ul") {
+                if ul["class"] == "mui-collection mui-collection--grid desktop-track-collection mui-collection--medium" {
+                    for a in ul.css("a") {
+                        if a["class"] == "mui-cell mui-cell--sm" {
+                            for div in a.css("div") {
+                                if div["class"] == "cover-container" {
+                                    var link = div["style"] ?? ""
+                                    link.removeFirst(22)
+                                    link.removeLast(2)
+                                    print(link)
+                                }
+                                
+                                if div["class"] == "track-info" {
+                                    print("https://www.musixmatch.com\(a["href"] ?? "")")
+                                    for span in div.css("span") {
+                                        if span["class"] == "title" {
+                                            print("Nombre de la canción: \(span.text?.trim ?? "")")
+                                        }
+                                        
+                                        if span["class"] == "artist-name" {
+                                            print("Nombre del artista: \(span.text?.trim ?? "")")
+                                        }
+                                    }
+                                    
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } catch {
+            print(error)
+        }
+    }
 
 }
 
+extension String {
+    var trim: String {
+        return self.trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+}
