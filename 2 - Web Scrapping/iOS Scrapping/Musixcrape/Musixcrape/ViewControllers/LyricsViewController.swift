074d93f7ed06b68dd0f6e066edc4fb0aa2e427d4
Moisés Córdova <kernel_linux@outlook.com>
2019-10-09 16:54:51 -0500

    Added Lyrics ViewController

---
+++ 2 - Web Scrapping/iOS Scrapping/Musixcrape/Musixcrape/ViewControllers/LyricsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LyricsViewController.swift
+//  Musixcrape
+//
+//  Created by Moisés Córdova on 09/10/19.
+//  Copyright © 2019 Moisés Córdova. All rights reserved.
+//
+
+import UIKit
+
+class LyricsViewController: UIViewController {
+
+    @IBOutlet weak var albumImageView: UIImageView!
+    @IBOutlet weak var albumLabel: UILabel!
+    @IBOutlet weak var artistLabel: UILabel!
+    @IBOutlet weak var lyricsTextView: UITextView!
+    //lyricsVC
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func doneButtonPressed(_ sender: UIButton) {
+    }
+    
+}
